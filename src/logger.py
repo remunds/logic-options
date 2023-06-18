b5d7d104977809d2e6b81a62a627c965e16f332f
MaggiR <mark.rothermel@web.de>
2023-06-18 17:48:10 +0200

    Improve logging output text format

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,12 @@
 import logging
 import os
+import sys
 import time
 
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
+
+from utils import sec2hhmmss, num2text
 
 
 class Logger:
@@ -11,7 +14,9 @@ class Logger:
         self.target_dir = log_dir
         self.tf_writer = None
         self.start_time = time.time()
-        self.n_eps = 0
+        self.episode_cnt = 0
+
+        self.return_record = 0
 
         if not os.path.exists(self.target_dir):
             os.makedirs(self.target_dir)
@@ -27,20 +32,33 @@ class Logger:
         file_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
 
+        # Output logging stream to stdout instead of stderr to avoid red output color in PyCharm
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
     def log_episode(self, steps, ret, option_lengths, ep_steps, epsilon):
-        self.n_eps += 1
-        self.logger.info(f"> ep {self.n_eps} done. total_steps={steps} | reward={ret} | episode_steps={ep_steps} "
-                         f"| hours={(time.time() - self.start_time) / 60 / 60:.3f} | epsilon={epsilon:.3f}")
-        self.writer.add_scalar(tag="returns", scalar_value=ret, global_step=self.n_eps)
-        self.writer.add_scalar(tag='episode_lengths', scalar_value=ep_steps, global_step=self.n_eps)
+        self.episode_cnt += 1
+
+        if ret > self.return_record:
+            self.logger.info("New record!")
+            self.return_record = ret
+
+        self.logger.info(f"> ep {self.episode_cnt} done. "
+                         f"total_steps={num2text(steps)} "
+                         f"| return={ret} "
+                         f"| episode_steps={ep_steps} "
+                         f"| time={sec2hhmmss(self.get_passed_time())} "
+                         f"| epsilon={epsilon:.3f}")
+
+        self.writer.add_scalar(tag="returns", scalar_value=ret, global_step=self.episode_cnt)
+        self.writer.add_scalar(tag='episode_lengths', scalar_value=ep_steps, global_step=self.episode_cnt)
 
         # Keep track of options statistics
         for option, lens in option_lengths.items():
             # Need better statistics for this one, point average is terrible in this case
             self.writer.add_scalar(tag=f"option_{option}_avg_length",
-                                   scalar_value=np.mean(lens) if len(lens) > 0 else 0, global_step=self.n_eps)
+                                   scalar_value=np.mean(lens) if len(lens) > 0 else 0, global_step=self.episode_cnt)
             self.writer.add_scalar(tag=f"option_{option}_active", scalar_value=sum(lens) / ep_steps,
-                                   global_step=self.n_eps)
+                                   global_step=self.episode_cnt)
 
     def log_data(self, step, actor_loss, critic_loss, entropy, epsilon):
         if actor_loss:
@@ -49,3 +67,7 @@ class Logger:
             self.writer.add_scalar(tag="critic_loss", scalar_value=critic_loss.item(), global_step=step)
         self.writer.add_scalar(tag="policy_entropy", scalar_value=entropy, global_step=step)
         self.writer.add_scalar(tag="epsilon", scalar_value=epsilon, global_step=step)
+
+    def get_passed_time(self):
+        """Returns the passed time since initialization, in seconds."""
+        return time.time() - self.start_time
