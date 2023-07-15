Add input normalization, fix bug in cases where no object of a category is visible on screen, refactoring, deactivate hud for OC representation

src/option_critic.py
@@ -16,7 +16,8 @@ from torch.distributions import Categorical, Bernoulli
 
 from experience_replay import ReplayBuffer
 from logger import Logger
-from utils import to_tensor, objects_to_numeric_vector, categorize_objects_into_dict, get_category_counts, pad_object_list
+from utils import to_tensor, objects_to_matrix, categorize_objects_into_dict, get_category_counts, \
+    pad_object_list, get_env_name, normalize_object_matrix
 from param_schedule import ParamScheduler
 
 MODEL_BASE_PATH = "out/models/"
@@ -104,6 +105,8 @@ class OptionCritic(nn.Module):
         self.train(not testing)
 
         self.running = True
+        self.transitions_total = 0
+        self.return_record = -np.inf
 
     def _initialize_latent_model(self, is_object_centric):
         if is_object_centric:
@@ -134,7 +137,7 @@ class OptionCritic(nn.Module):
         return state
 
     def practice(self,
-                 env,
+                 env: Union[Env, OCAtari],
                  num_transitions: int = 4e6,
                  learning_rate: float = 0.0005,
                  optimizer: str = 'Adam',
@@ -167,8 +170,6 @@ class OptionCritic(nn.Module):
         :return:
         """
 
-        # initialize_tensorboard()
-
         if epsilon is None:
             epsilon = ParamScheduler(0, "const")
         else:
@@ -190,7 +191,7 @@ class OptionCritic(nn.Module):
         torch.manual_seed(seed)  # TODO: deprecated
 
         buffer = ReplayBuffer(capacity=replay_buffer_length, seed=seed)
-        logger = Logger(log_dir=self.model_dir + "log")
+        logger = Logger(log_dir=self.model_dir + "/log")
 
         transition = 0
 
@@ -204,7 +205,7 @@ class OptionCritic(nn.Module):
 
             state, _ = env.reset()
             if self.object_centric:
-                state = objects_to_numeric_vector(pad_object_list(env.objects, self.max_object_counts))
+                state = self.get_current_oc_env_state(env)
 
             greedy_option = self.choose_option_greedy(state)
             current_option = 0
@@ -228,7 +229,13 @@ class OptionCritic(nn.Module):
                 # Perform transition
                 next_state, reward, done, _, _ = env.step(action)
                 if self.object_centric:
-                    next_state = objects_to_numeric_vector(pad_object_list(env.objects, self.max_object_counts))
+                    next_state = self.get_current_oc_env_state(env)
+
+                # Reward shaping for Pong
+                # if "Pong" in env.game_name:
+                #     ball_position_y = env.objects[1].y
+                #     player_position_y = env.objects[0].y
+                #     reward += 0.05 / (1 + abs(ball_position_y - player_position_y))
 
                 # Save transition
                 buffer.push(state, current_option, reward, next_state, done)
@@ -257,17 +264,27 @@ class OptionCritic(nn.Module):
 
                 terminate_option, greedy_option = self.predict_option_termination(next_state, current_option)
 
+                self.transitions_total += 1
                 transition += 1
                 episode_length += 1
                 current_option_length += 1
                 state = next_state
 
-                logger.log_data(transition, actor_loss, critic_loss, entropy.item(), eps)
+                if self.transitions_total % SUMMARY_WRITE_INTERVAL == 0:
+                    logger.log_data(self.transitions_total, actor_loss, critic_loss, entropy.item(), eps)
 
-            logger.log_episode(transition, ret, option_lengths, episode_length, eps)
+                if self.transitions_total % CHECKPOINT_SAVE_PERIOD == 0:
+                    self.save()
+
+            logger.log_episode(self.transitions_total, ret, option_lengths, episode_length, eps)
+
+            if ret > self.return_record:
+                if self.return_record > -np.inf:
+                    logger.logger.info(f"New record: {ret}")
+                    self.save(suffix="_record_%.0f" % ret)
+                self.return_record = ret
 
         self.save()
-        print("Saved model.")
 
     def play(self, env, max_episode_length: int = np.inf):
         """Let agent interact with environment and render."""
@@ -294,6 +311,8 @@ class OptionCritic(nn.Module):
             ret = 0  # return (= sum of all rewards)
 
             state, _ = env.reset()
+            if self.object_centric:
+                state = self.get_current_oc_env_state(env)
             greedy_option = self.choose_option_greedy(state)
             current_option = 0
 
@@ -311,6 +330,8 @@ class OptionCritic(nn.Module):
 
                 # Perform transition
                 next_state, reward, done, _, _ = env.step(action)
+                if self.object_centric:
+                    next_state = self.get_current_oc_env_state(env)
                 ret += reward
 
                 # Render environment for human

src/utils.py
@@ -3,41 +3,20 @@ from __future__ import annotations
 import gymnasium
 import numpy as np
 import torch
-from typing import Sequence
+from typing import Sequence, Union
 
 from ocatari.core import OCAtari
 from ocatari.ram.game_objects import GameObject
 from gymnasium.wrappers import AtariPreprocessing, TransformReward
-from gymnasium.wrappers import FrameStack as FrameStack_
+from gymnasium.wrappers import FrameStack  # as FrameStack_
 from tensorboard import program
 
 from fourrooms import Fourrooms
 
 
-class LazyFrames(object):
-    def __init__(self, frames):
-        self._frames = frames
-
-    def __array__(self, dtype=None):
-        out = np.concatenate(self._frames, axis=0)
-        if dtype is not None:
-            out = out.astype(dtype)
-        return out
-
-    def __len__(self):
-        return len(self.__array__())
-
-    def __getitem__(self, i):
-        return self.__array__()[i]
-
-
-class FrameStack(FrameStack_):
-    def __init__(self, env, k):
-        FrameStack_.__init__(self, env, k)
-
-    def _get_ob(self):
-        assert len(self.frames) == self.k
-        return LazyFrames(list(self.frames))
+ATARI_SCREEN_WIDTH = 160
+ATARI_SCREEN_HEIGHT = 210
+AVG_SCREEN_VELOCITY = 4  # TODO: test
 
 
 def make_env(name, seed, render_mode=None, framestack=4,
@@ -57,20 +36,20 @@ def make_env(name, seed, render_mode=None, framestack=4,
         if not is_atari:
             raise NotImplemented("Object centric for non-Atari games not implemented yet.")
         env_name = name.split("/")[1].split("-")[0]  # Extract environment name from gymnasium env identifier
-        env = OCAtari(env_name, mode='revised', hud=True, render_mode=render_mode,
+        env = OCAtari(env_name, mode='revised', hud=False, render_mode=render_mode,
                       frameskip=gym_frameskip, **kwargs)
     else:
         env = gymnasium.make(name, render_mode=render_mode, frameskip=gym_frameskip, **kwargs)
 
-    if is_atari:
-        frameskip = 4 if frameskip is None else frameskip
-        env = AtariPreprocessing(env,
-                                 grayscale_obs=True,
-                                 scale_obs=True,
-                                 terminal_on_life_loss=True,
-                                 frame_skip=frameskip)
-        env = TransformReward(env, lambda r: float(np.clip(r, -1, 1)))
-        env = FrameStack(env, framestack)
+    # if is_atari:
+    frameskip = 4 if frameskip is None else frameskip
+    env = AtariPreprocessing(env,
+                             grayscale_obs=True,
+                             scale_obs=True,
+                             terminal_on_life_loss=True,
+                             frame_skip=frameskip)
+    env = TransformReward(env, lambda r: float(np.clip(r, -1, 1)))
+    env = FrameStack(env, framestack)
 
     # Initialize environment using seed
     env.reset(seed=seed, options={})
@@ -125,18 +104,36 @@ def sec2hhmmss(s):
     return "%d:%02d:%02d h" % (h, m % 60, s % 60)
 
 
-def objects_to_numeric_vector(objects: Sequence[GameObject]):
+def objects_to_matrix(objects: Sequence[GameObject]):
     """Converts a list of objects into the corresponding matrix representation.
     Each row (x, y, dx, dy) of the returned matrix represents the object's center (!)
     position and velocity vector."""
+
     object_matrix = np.zeros(shape=(len(objects), 4), dtype='float32')
+    object_matrix[:] = np.nan
+
     for i, obj in enumerate(objects):
         if obj is not None:
-            object_matrix[i] = (obj.x + obj.w // 2,
-                                obj.y + obj.h // 2,
+            object_matrix[i] = (obj.x + obj.w / 2,
+                                obj.y + obj.h / 2,
                                 obj.dx,
                                 obj.dy)
-    return object_matrix.flatten()
+
+    return object_matrix
+
+
+def normalize_object_matrix(object_matrix):
+    # Normalize positions to [-1, 1] TODO: this applies only to Atari => generalize
+    object_matrix[:, 0] = 2 * object_matrix[:, 0] / ATARI_SCREEN_WIDTH - 1
+    object_matrix[:, 1] = 2 * object_matrix[:, 1] / ATARI_SCREEN_HEIGHT - 1
+
+    # Normalize velocities with tanh to [-1, 1]
+    object_matrix[:, 2:4] = np.tanh(object_matrix[:, 2:4] / AVG_SCREEN_VELOCITY)
+
+    # Replace NaNs with zeros
+    np.nan_to_num(object_matrix, nan=0, copy=False)
+
+    return object_matrix
 
 
 def categorize_objects_into_dict(objects: Sequence[GameObject]):
@@ -155,13 +152,18 @@ def get_category_counts(objects_categorized: dict['str', Sequence[GameObject]]):
 
 
 def pad_object_list(objects: Sequence[GameObject], max_object_counts: dict):
+    """Takes a list of objects and fills it with None entries where other
+    objects are missing (compared to max_object_counts)."""
     padded_object_list = []
     objects_categorized = categorize_objects_into_dict(objects)
 
     # Construct padded object list iteratively by category
     for category in list(max_object_counts.keys()):
-        category_objects = objects_categorized[category]
-        padded_object_list.extend(category_objects)
+        if category in objects_categorized.keys():
+            category_objects = objects_categorized[category]
+            padded_object_list.extend(category_objects)
+        else:
+            category_objects = []
 
         # Add padding for this category
         padding_length = max_object_counts[category] - len(category_objects)
@@ -176,3 +178,7 @@ def initialize_tensorboard():
     tb.configure(argv=[None, '--logdir', 'out/models'])
     url = tb.launch()
     print(f"Tensorboard listening on {url}")
+
+
+def get_env_name(env: Union[gymnasium.Env, OCAtari]):
+    return env.spec.name if isinstance(env, gymnasium.Env) else env.game_name