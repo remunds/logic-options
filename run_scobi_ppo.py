6db13952947d79236dff4cb032474feed8221c90
MaggiR <mark.rothermel@web.de>
2023-08-21 21:17:36 +0200

    Update run_scobi_ppo.py with experiment queueing

--- run_scobi_ppo.py
+++ run_scobi_ppo.py
@@ -1,15 +1,18 @@
 from pathlib import Path
+import shutil
+import os
 
 import yaml
 from stable_baselines3 import PPO
 from stable_baselines3.common.logger import configure
 import torch as th
 
-from utils import maybe_make_schedule, get_experiment_name_from_hyperparams, init_envs, get_torch_device
+from utils import maybe_make_schedule, get_experiment_name_from_hyperparams, init_envs, get_torch_device, \
+    get_atari_identifier
 from callbacks import init_callbacks, EvalCallback
 
-CONFIG_PATH = "in/config/scobi.yaml"
-MODELS_PATH = "out/scobi_sb3/"
+QUEUE_PATH = "in/queue/"
+OUT_BASE_PATH = "out/rl-zoo/"
 
 
 def run(name: str = None,
@@ -18,12 +21,17 @@ def run(name: str = None,
         seed: int = 0,
         environment: dict = None,
         model: dict = None,
-        training: dict = None):
+        training: dict = None,
+        config_path: str = ""):
     th.manual_seed(seed)
+
+    game_identifier = get_atari_identifier(environment["name"])
 
     # Set experiment name
     if name is None:
         name = get_experiment_name_from_hyperparams(environment_kwargs=environment, seed=seed)
+
+    model_path = Path(OUT_BASE_PATH, game_identifier, name)
 
     device = get_torch_device(cuda)
 
@@ -32,8 +40,8 @@ def run(name: str = None,
     n_envs = cores
     n_eval_envs = cores
     total_timestamps = int(float(training["total_timesteps"]))
-    log_path = Path(MODELS_PATH, name, "logs")
-    ckpt_path = Path(MODELS_PATH, name, "checkpoints")
+    log_path = model_path / "logs"
+    ckpt_path = model_path / "checkpoints"
     log_path.mkdir(parents=True, exist_ok=True)
     ckpt_path.mkdir(parents=True, exist_ok=True)
 
@@ -44,7 +52,7 @@ def run(name: str = None,
                              object_centric=object_centric,
                              n_envs=n_envs,
                              eval_env=eval_env,
-                             n_eval_episodes=4*n_eval_envs,
+                             n_eval_episodes=4 * n_eval_envs,
                              ckpt_path=ckpt_path,
                              eval_callback_cls=EvalCallback,
                              eval_frequency=eval_frequency)
@@ -66,12 +74,20 @@ def run(name: str = None,
     new_logger = configure(str(log_path), ["tensorboard"])
     model.set_logger(new_logger)
 
+    # Save config file for documentation
+    shutil.copy(src=config_path, dst=model_path / "config.yaml")
+
     print(f"Experiment name: {name}")
     print(f"Started {type(model).__name__} training with {n_envs} actors and {n_eval_envs} evaluators...")
     model.learn(total_timesteps=total_timestamps, callback=cb_list)
 
 
 if __name__ == "__main__":
-    with open(CONFIG_PATH, "r") as f:
-        config = yaml.load(f, Loader=yaml.Loader)
-    run(**config)
+    config_files = os.listdir(QUEUE_PATH)
+    while len(config_files) > 0:
+        config_path = QUEUE_PATH + config_files[0]
+        with open(config_path, "r") as f:
+            config = yaml.load(f, Loader=yaml.Loader)
+        run(config_path=config_path, **config)
+        os.remove(config_path)
+        config_files = os.listdir(QUEUE_PATH)
