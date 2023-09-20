3781ead0976a33796a0606c04c6e7dc9394e74f0
MaggiR <mark.rothermel@web.de>
2023-09-20 17:26:46 +0200

    Add eval script

---
+++ eval.py
@@ -0,0 +1,19 @@
+from options.ppo import load_agent
+from utils.callbacks import evaluate_policy
+
+
+if __name__ == "__main__":
+    name = "debug"
+    env_name = "ALE/Seaquest-v5"
+    n_envs = 4
+    n_eval_episodes = 100
+
+    model = load_agent(name, env_name, n_envs=n_envs)
+    env = model.get_env()
+
+    print(f"Evaluating '{name}' on {env_name} for {n_eval_episodes} episodes... ", end="")
+
+    mean_return, std_return = evaluate_policy(model, env, n_eval_episodes=n_eval_episodes, deterministic=True)
+
+    print(f"Done.")
+    print(f"\tReturn: \t{mean_return:.2f} +/- {std_return:.2f}")
