3c0d5b63c7923db20725d25f829f609e8c798a32
MaggiR <mark.rothermel@web.de>
2023-10-21 21:50:12 +0200

    Add screenshot feature

--- src/envs/play_env.py
+++ src/envs/play_env.py
@@ -1,11 +1,15 @@
 import pygame
 from meeting_room import MeetingRoom
+from datetime import datetime
+
+
+SCREENSHOTS_BASE_PATH = "../../out/screenshots/"
 
 
 if __name__ == "__main__":
     env = MeetingRoom(render_mode="human", walls_fixed=False)
     env.metadata['video.frames_per_second'] = 60
-    env.reset(seed=42)
+    env.reset()
     keys2actions = env.get_keys_to_action()
 
     running = True
@@ -19,6 +23,9 @@ if __name__ == "__main__":
             elif event.type == pygame.KEYDOWN:  # keyboard key pressed
                 if event.key == pygame.K_r:  # 'R': reset
                     env.reset()
+                elif event.key == pygame.K_c:  # 'C': capture screenshot
+                    file_name = f"{datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')}.png"
+                    pygame.image.save(env.window, SCREENSHOTS_BASE_PATH + file_name)
                 else:
                     action = keys2actions.get((event.key,))
 
