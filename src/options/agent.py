Fix two bugs causing to miss normalization for pre-trained models, general refactoring

src/options/agent.py
@@ -91,6 +91,9 @@ class OptionsAgent(BasePolicy):
             self._init_predicate_conversion(device)
 
     def _init_predicate_conversion(self, device):
+        """Computes the pred2option np.array which converts predicate IDs into top-level
+        option position IDs."""
+
         assert isinstance(self.meta_policy, NudgePolicy)
         n_top_level_options = self.hierarchy_shape[0]
         pred2option = []
@@ -159,18 +162,16 @@ class OptionsAgent(BasePolicy):
     def n_policies(self):
         return np.sum(self.hierarchy_shape) + 1
 
-    def get_option_by_id(self, option_id: th.Tensor) -> OptionCollection:
+    def get_option_by_idx(self, index: th.Tensor) -> OptionCollection:
         """
-        Turns a batch of option IDs into the corresponding list of options
-
-        :param option_id: 2D matrix
-        :return:
+        Turns a batch of option indexes into the corresponding list of options
+        :param index: 2D matrix
         """
         options = []
-        for level, idx in option_id:
+        for level, position in index:
             if self.accepts_predicates and level == 0:
-                idx = self.pred2option[idx]
-            options.append(self.options_hierarchy[level][idx])
+                position = self.pred2option[position]
+            options.append(self.options_hierarchy[level][position])
         return OptionCollection(options)
 
     def set_training_mode(self, mode: bool) -> None:
@@ -191,18 +192,18 @@ class OptionsAgent(BasePolicy):
     def forward_option(
             self,
             obs: th.Tensor,
-            option_id: th.Tensor,
+            option_idx: th.Tensor,
             deterministic: bool = False
     ) -> Tuple[th.Tensor, th.Tensor, th.Tensor]:
         """
         Forward pass in actor and critic for specified option.
 
         :param obs: Observation
-        :param option_id: The target option's index [level, ID]
+        :param option_idx: The target option's index [level, position]
         :param deterministic: Whether to sample or use deterministic actions
         :return: action, value, and log probability of the action
         """
-        option = self.get_option_by_id(option_id)
+        option = self.get_option_by_idx(option_idx)
         return option.forward(obs, deterministic)
 
     def forward_all(
@@ -217,7 +218,7 @@ class OptionsAgent(BasePolicy):
         option.
 
         :param obs: Observation
-        :param option_traces:
+        :param option_traces: For each hierarchy level the position of the currently active option.
         :param option_terminations: Same shape as option_traces where each entry is True iff the
             corresponding option in option_traces has terminated.
         :param deterministic:
@@ -247,39 +248,39 @@ class OptionsAgent(BasePolicy):
         is_terminated = option_terminations[:, 0]
         options[is_terminated, 0] = new_options[is_terminated]
 
-        for env_id, trace in enumerate(option_traces):
-            env_obs = th.unsqueeze(obs[env_id], dim=0)
+        for env, trace in enumerate(option_traces):
+            env_obs = th.unsqueeze(obs[env], dim=0)
 
             # Determine new options (if needed)
-            for level_id, option_id in enumerate(trace):
-                if level_id == 0:
+            for level in range(len(trace)):
+                if level == 0:
                     continue
 
-                higher_level_option_id = options[env_id][level_id - 1]
-                active_option = self.options_hierarchy[level_id - 1][higher_level_option_id]
+                higher_level_option_pos = options[env][level - 1]
+                active_option = self.options_hierarchy[level - 1][higher_level_option_pos]
 
-                option, values[env_id][level_id], log_probs[env_id][level_id] = \
+                option, values[env][level], log_probs[env][level] = \
                     active_option(env_obs, deterministic)
 
                 # Replace terminated options
-                if option_terminations[env_id][level_id]:
-                    options[env_id][level_id] = option
+                if option_terminations[env][level]:
+                    options[env][level] = option
 
             # Determine new action (always executed)
-            lowest_level_option_id = options[env_id][-1]
-            lowest_level_option = self.options_hierarchy[-1][lowest_level_option_id]
-            actions[env_id], values[env_id, -1], log_probs[env_id, -1] = lowest_level_option(env_obs, deterministic)
+            lowest_level_option_pos = options[env][-1]
+            lowest_level_option = self.options_hierarchy[-1][lowest_level_option_pos]
+            actions[env], values[env, -1], log_probs[env, -1] = lowest_level_option(env_obs, deterministic)
 
         return (options, actions), values, log_probs
 
     def forward_option_terminator(
             self,
             obs: th.Tensor,
-            option_id: th.Tensor,
+            option_idx: th.Tensor,
             deterministic: bool = False
     ) -> Tuple[th.Tensor, th.Tensor]:
         """Forward in the terminator of specified options."""
-        options = self.get_option_by_id(option_id)
+        options = self.get_option_by_idx(option_idx)
         return options.forward_terminator(obs, deterministic)
 
     def predict_all_values(self, obs: th.Tensor, option_traces: th.Tensor) -> th.Tensor:
@@ -292,11 +293,11 @@ class OptionsAgent(BasePolicy):
 
         values = th.zeros(option_traces.shape[0], option_traces.shape[1] + 1)
         values[:, 0] = self.meta_policy.predict_values(obs).squeeze()
-        for env_id, trace in enumerate(option_traces):
-            env_obs = th.unsqueeze(obs[env_id], dim=0)
-            for level_id, option_id in enumerate(trace):
-                option = self.options_hierarchy[level_id][option_id]
-                values[env_id, level_id + 1] = option._policy.predict_values(env_obs)
+        for env, trace in enumerate(option_traces):
+            env_obs = th.unsqueeze(obs[env], dim=0)
+            for level, position in enumerate(trace):
+                option = self.options_hierarchy[level][position]
+                values[env, level + 1] = option.predict_values(env_obs)
         return values
 
     def forward_all_terminators(
@@ -316,25 +317,25 @@ class OptionsAgent(BasePolicy):
 
         for env_id, trace in enumerate(option_traces):
             env_obs = th.unsqueeze(obs[env_id], dim=0)
-            for level, option_id in enumerate(trace):
-                option = self.options_hierarchy[level][option_id]
-                termination, log_prob = option._terminator.forward(env_obs, deterministic)
+            for level, position in enumerate(trace):
+                option = self.options_hierarchy[level][position]
+                termination, log_prob = option.forward_terminator(env_obs, deterministic)
                 terminations[env_id, level] = termination
                 log_probs[env_id, level] = log_prob
 
         return terminations, log_probs
 
-    def get_option_termination_dist(self, obs: th.Tensor, option_id: th.Tensor) -> CategoricalDistribution:
-        option = self.get_option_by_id(option_id)
+    def get_option_termination_dist(self, obs: th.Tensor, option_idx: th.Tensor) -> CategoricalDistribution:
+        option = self.get_option_by_idx(option_idx)
         return option.get_termination_dist(obs)
 
     def evaluate_option_terminations(
             self,
             obs: th.Tensor,
-            option_id: th.Tensor,
+            option_idx: th.Tensor,
             terminations: th.Tensor
     ) -> th.Tensor:
-        option = self.get_option_by_id(option_id)
+        option = self.get_option_by_idx(option_idx)
         return option.evaluate_terminations(obs=obs, terminations=terminations)
 
     def _get_constructor_parameters(self) -> Dict[str, Any]:

src/options/hierarchy.py
@@ -45,11 +45,12 @@ class OptionsHierarchy(nn.Module):
         self._build_attributes()
 
     def _build_attributes(self):
-        """Adds for each option a corresponding attribute to this OptionsHierarchy object.
-        These attributes are needed for saving and loading the model."""
+        """Adds for each option a corresponding attribute to this OptionsHierarchy object
+        containing the respective option object. They are needed for saving and loading
+        the model."""
         for level_id, level in enumerate(self.options):
-            for option_id, option in enumerate(level):
-                name = get_option_name(level_id, option_id)
+            for option_pos, option in enumerate(level):
+                name = get_option_name(level_id, option_pos)
                 setattr(self, name, option)
 
     def __getitem__(self, item) -> List[Option]:

src/options/ppo.py
@@ -12,7 +12,7 @@ from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.utils import obs_as_tensor, explained_variance, update_learning_rate
 from stable_baselines3.common.vec_env import VecEnv
 from stable_baselines3.ppo.ppo import PPO
-from torch.nn import functional as F
+from torch.nn.functional import mse_loss
 from tqdm import tqdm
 
 from options.agent import OptionsAgent
@@ -40,6 +40,8 @@ class OptionsPPO(PPO):
     rollout_buffer: OptionsRolloutBuffer
     policy: OptionsAgent
     progress: tqdm
+    _last_active_options: np.array
+    _last_option_terminations: np.array
 
     def __init__(self,
                  policy_ent_coef: float = 0,
@@ -220,8 +222,8 @@ class OptionsPPO(PPO):
             level_terminations = self.rollout_buffer.option_terminations[:, :, level].astype(bool)
             dones = self.rollout_buffer.dones.astype(bool)
 
-            for option_id in range(n_options):
-                option_active = level_options == option_id
+            for position in range(n_options):
+                option_active = level_options == position
                 option_count = np.sum(option_active)
                 option_activity_share = option_count / self.rollout_buffer.total_transitions
 
@@ -232,7 +234,7 @@ class OptionsPPO(PPO):
                 else:
                     option_length = np.nan
 
-                option_name = get_option_name(level, option_id)
+                option_name = get_option_name(level, position)
                 self.logger.record(option_name + "/activity_share", option_activity_share)
                 self.logger.record(option_name + "/length", option_length)
 
@@ -257,9 +259,9 @@ class OptionsPPO(PPO):
 
         # Train actor, critic, and terminator of each option
         for level, n_options in enumerate(self.policy.hierarchy_shape):
-            for option_id in range(n_options):
-                self._train_actor_critic(level, option_id)
-                self._train_terminator(level, option_id)
+            for position in range(n_options):
+                self._train_actor_critic(level, position)
+                self._train_terminator(level, position)
 
         self._n_updates += self.n_epochs
 
@@ -272,14 +274,14 @@ class OptionsPPO(PPO):
 
         self.progress.close()
 
-    def _train_actor_critic(self, level: int = -1, option_id: int = None) -> None:
+    def _train_actor_critic(self, level: int = -1, position: int = None) -> None:
         """Trains the actor and critic of the meta policy (level = -1) or, if specified, of
         the option."""
         if level == -1:  # meta-policy
             policy = self.policy.meta_policy
             evaluate_fn = policy.evaluate_actions
         else:
-            option = self.policy.options_hierarchy[level][option_id]
+            option = self.policy.options_hierarchy[level][position]
             if not option.policy_trainable:
                 return
             policy = option.get_policy()
@@ -299,7 +301,7 @@ class OptionsPPO(PPO):
         for epoch in range(self.n_epochs):
             approx_kl_divs = []
             # Do a complete pass on the rollout buffer
-            for rollout_data in self.rollout_buffer.get_actor_critic_train_data(level, option_id, self.batch_size):
+            for rollout_data in self.rollout_buffer.get_actor_critic_train_data(level, position, self.batch_size):
                 options_actions = rollout_data.options_actions
                 if isinstance(policy.action_space, spaces.Discrete):
                     options_actions = options_actions.long().flatten()
@@ -337,7 +339,7 @@ class OptionsPPO(PPO):
                         values - old_values, -clip_range_vf, clip_range_vf
                     )
                 # Value loss using the TD(gae_lambda) target
-                value_loss = F.mse_loss(rollout_data.returns, values_pred)
+                value_loss = mse_loss(rollout_data.returns, values_pred)
                 value_losses.append(value_loss.item())
 
                 # Entropy loss favor exploration
@@ -381,7 +383,7 @@ class OptionsPPO(PPO):
         # Logs
         if loss is not None:
             print(f" Loss: {loss:.3f}", end="")
-            policy_name = "meta_policy" if level == -1 else get_option_name(level, option_id)
+            policy_name = "meta_policy" if level == -1 else get_option_name(level, position)
             base_str = f"{policy_name}/actor_critic/"
             self.logger.record(base_str + "loss", loss.item())
             self.logger.record(base_str + "policy_loss", np.mean(pg_losses))
@@ -389,17 +391,17 @@ class OptionsPPO(PPO):
             self.logger.record(base_str + "entropy_loss", np.mean(entropy_losses))
             self.logger.record(base_str + "approx_kl", np.mean(approx_kl_divs))
             self.logger.record(base_str + "clip_fraction", np.mean(clip_fractions))
-            values = self.rollout_buffer.get_values(level, option_id)
-            returns = self.rollout_buffer.get_returns(level, option_id)
+            values = self.rollout_buffer.get_values(level, position)
+            returns = self.rollout_buffer.get_returns(level, position)
             explained_var = explained_variance(values.flatten(), returns.flatten())
             # assert not np.isnan(explained_var)
             self.logger.record(base_str + "explained_variance", explained_var)
             if hasattr(policy, "log_std"):
                 self.logger.record(base_str + "std", th.exp(policy.log_std).mean().item())
 
-    def _train_terminator(self, level: int, option_id: int) -> None:
+    def _train_terminator(self, level: int, position: int) -> None:
         """Trains the terminator of each specified option."""
-        option = self.policy.options_hierarchy[level][option_id]
+        option = self.policy.options_hierarchy[level][position]
         if not option.terminator_trainable:
             return
         terminator: Terminator = option.get_terminator()
@@ -417,7 +419,7 @@ class OptionsPPO(PPO):
         for epoch in range(self.n_epochs):
             approx_kl_divs = []
             # Do a complete pass on the rollout buffer
-            for rollout_data in self.rollout_buffer.get_terminator_train_data(level, option_id, self.batch_size):
+            for rollout_data in self.rollout_buffer.get_terminator_train_data(level, position, self.batch_size):
                 next_obs = rollout_data.next_observations
                 terminations = rollout_data.option_terminations
                 tn_log_probs, entropy = evaluate_fn(next_obs, terminations)
@@ -470,7 +472,7 @@ class OptionsPPO(PPO):
 
         # Logs
         if loss is not None:
-            policy_name = get_option_name(level, option_id)
+            policy_name = get_option_name(level, position)
             base_str = f"{policy_name}/terminator/"
             self.logger.record(base_str + "entropy_loss", np.mean(entropy_losses))
             self.logger.record(base_str + "termination_loss", np.mean(losses))

src/options/rollout_buffer.py
@@ -147,7 +147,7 @@ class OptionsRolloutBuffer(BaseBuffer):
     def get_actor_critic_train_data(
             self,
             level: int = -1,
-            option_id: int = None,
+            position: int = None,
             batch_size: Optional[int] = None
     ) -> Generator[ActorCriticSamples, None, None]:
         assert self.full, "Buffer is not full"
@@ -155,7 +155,7 @@ class OptionsRolloutBuffer(BaseBuffer):
         if not self.generator_ready:
             self._prepare_generator()
 
-        decisions = self.get_decisions(level, option_id)
+        decisions = self.get_decisions(level, position)
 
         indices = np.where(decisions)[0]
 
@@ -164,7 +164,7 @@ class OptionsRolloutBuffer(BaseBuffer):
     def get_terminator_train_data(
             self,
             level: int,
-            option_id: int,
+            position: int,
             batch_size: Optional[int] = None
     ) -> Generator[TerminatorSamples, None, None]:
         assert self.full, "Buffer is not full"
@@ -172,7 +172,7 @@ class OptionsRolloutBuffer(BaseBuffer):
         if not self.generator_ready:
             self._prepare_generator()
 
-        option_active = self.get_option_active(level, option_id)
+        option_active = self.get_option_active(level, position)
         indices = np.where(option_active)[0]
 
         return self._get(indices, level, self._get_terminator_samples, batch_size)
@@ -271,12 +271,12 @@ class OptionsRolloutBuffer(BaseBuffer):
     def get_option_active(
             self,
             level: Union[int, th.Tensor],
-            option_id: Union[int, th.Tensor]
+            position: Union[int, th.Tensor]
     ) -> th.Tensor:
         """Returns the indices for all transitions where the specified option was active,
         i.e., is contained in an option trace."""
         assert self.generator_ready
-        return self.option_traces[:, level] == option_id
+        return self.option_traces[:, level] == position
 
     def get_option_starts(self, level: Union[int, th.Tensor] = -1):
         assert isinstance(level, int)
@@ -289,7 +289,7 @@ class OptionsRolloutBuffer(BaseBuffer):
         option_starts[1:] = self.option_terminations[:-1, ..., level]
         return option_starts
 
-    def get_decisions(self, level: Union[int, th.Tensor], option_id: Union[int, th.Tensor]) -> np.array:
+    def get_decisions(self, level: Union[int, th.Tensor], position: Union[int, th.Tensor]) -> np.array:
         """The returned Boolean vector indicates at which time step the specified
         policy/option made a decision. True iff decision made."""
         assert isinstance(level, int)
@@ -297,7 +297,7 @@ class OptionsRolloutBuffer(BaseBuffer):
         if level == -1:  # meta policy
             active = np.ones(self.advantages.shape[0], dtype=bool)  # meta policy is always active
         else:
-            active = self.get_option_active(level, option_id)
+            active = self.get_option_active(level, position)
 
         if level + 1 < self.option_hierarchy_size:
             lower_level_starts = self.get_option_starts(level + 1)
@@ -315,14 +315,14 @@ class OptionsRolloutBuffer(BaseBuffer):
         next_local_value = self.next_values[..., 1:]  # in context of this option
         return termination_prob * next_outer_value + (1 - termination_prob) * next_local_value
 
-    def get_values(self, level: Union[int, th.Tensor] = -1, option_id: Union[int, th.Tensor] = None):
+    def get_values(self, level: Union[int, th.Tensor] = -1, position: Union[int, th.Tensor] = None):
         assert isinstance(level, int)
-        decisions = self.get_decisions(level, option_id)
+        decisions = self.get_decisions(level, position)
         return self.values[decisions, level+1]
 
-    def get_returns(self, level: Union[int, th.Tensor] = -1, option_id: Union[int, th.Tensor] = None):
+    def get_returns(self, level: Union[int, th.Tensor] = -1, position: Union[int, th.Tensor] = None):
         assert isinstance(level, int)
-        decisions = self.get_decisions(level, option_id)
+        decisions = self.get_decisions(level, position)
         return self.returns[decisions, level+1]
 
     def compute_returns_and_advantage(self, dones: np.ndarray) -> None:

src/utils/render.py
@@ -10,7 +10,7 @@ ROT_MATRIX = np.array([[0, -1], [1, 0]])
 
 def render_options_overlay(image, option_trace: List[int] = None, fps=None) -> None:
     """Displays an RGB pixel image using pygame.
-        Optional: Show the currently used option_id and/or the detected objects
+        Optional: Show the currently used option_idx and/or the detected objects
         and their velocities."""
 
     if fps is None:
@@ -36,8 +36,8 @@ def render_options_overlay(image, option_trace: List[int] = None, fps=None) -> N
     # If given, render option ID in top right corner
     if option_trace is not None and len(option_trace) > 0:
         option_text = f"Option {option_trace[0]}"
-        for option_id in option_trace[1:]:
-            option_text += "-%d" % option_id
+        for position in option_trace[1:]:
+            option_text += "-%d" % position
         draw_label(surface, option_text, (10, surface.get_size()[1] - 50),
                    font=pygame.font.SysFont('Source Code Pro', 30),
                    text_color=(0, 0, 0), bg_color=(255, 255, 255))