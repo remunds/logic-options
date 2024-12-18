bfeb9c6609ca07088242e41a7eac0ac738ba1d16
remunds <raban.emunds@gmail.com>
2024-12-18 12:43:18 +0100

    u

--- play.py
+++ play.py
@@ -1,13 +1,17 @@
 from logic_options.envs.renderer import Renderer
 
 if __name__ == "__main__":
-    # renderer = Renderer(env_name="ALE/Kangaroo-v5",
-    #                     agent_name="kangaroo_ill_def_rerun_8",
-    renderer = Renderer(env_name="MeetingRoom",
-                        agent_name="neural_pretrained_always_term",
+     renderer = Renderer(env_name="ALE/Seaquest-v5",
+                        #renderer = Renderer(env_name="MeetingRoom",
+                        #agent_name="neural_pretrained_maggi_trainable_1",
+                        #agent_name="neural_hierarchy_polterm_weak_very_low_ent",
+                        agent_name="logic_hierarchy_seaquest",
+                        #agent_name="neural_hierarchy_polterm_none_1",
+                        #agent_name="elevator_option_15",
+                        #agent_name="target_option_10",
                         fps=15,
                         deterministic=True,
-                        shadow_mode=True,
-                        wait_for_input=True,
+                        shadow_mode=False,
+                        wait_for_input=False,
                         render_predicate_probs=False)
-    renderer.run()
+     renderer.run()
